1e74949b973843152e49bb08fbee93dd4956994a
Sean Cooper <scooper@adaptavist.com>
2023-10-08 00:46:32 +0100

    Add QA assistant

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import os
 
 # from autogen import GroupChat, GroupChatManager
 
-seed = 10
+seed = 666
 working_dir = f'out_{seed}'
 master_plan_file = 'master-plan.txt'
 main_script_file = 'main.py'
@@ -45,6 +45,20 @@ def write_file(txt, name):
     with open(f'{working_dir}/{name}', "w") as plan_file:
         plan_file.write(txt)
 
+
+# initial_prompt = """We're building a RESTful API for managing customer data.
+# We will handle POST and GET requests to /customer and GET, PUT, DELETE /customer/:uuid requests.
+# Customers will have uuid, email, username, createdAt, updatedAt fields.
+# We will write all of the tests for each endpoint before we write the handlers.
+# We must test the body response data before we test http status codes in unit tests, this is vitally important.
+# We will run tests the tests individually as we go to prove the api works as expected.
+# We must prove the api endpoint works by running our unittest testing suite.
+# We will use Fastapi, unittest and uuidv7 time-ordered UUID.
+# We will update an in-memory db on create, update and delete requests.
+# We must never run the server directly, this is very important.
+# We must make sure the fastapi app has all of the routes defined correctly.
+# We must not create a venv or any other virtual environment.
+# Start by planning the application step by step, then implement each step."""
 
 initial_prompt = """We're building a RESTful API for managing customer data. 
 We will handle POST and GET requests to /customer and GET, PUT, DELETE /customer/:uuid requests.
@@ -116,10 +130,105 @@ user_proxy = autogen.UserProxyAgent(
     function_map={"ask_dev_consultant": ask_dev_consultant},
 )
 
-user_proxy.initiate_chat(
-    assistant,
-    message=initial_prompt
-)
+# user_proxy.initiate_chat(
+#     assistant,
+#     message=initial_prompt
+# )
 
 # TODO: review the final code by providing agents with the project structure and
 #       a way to inspect any file they want to read.
+
+def get_files_in_directory(directory):
+    file_list = []
+    for root, dirs, files in os.walk(directory):
+        if root != working_dir:
+            continue
+        for file in files:
+            file_list.append(file)
+    return file_list
+
+
+def open_file(filename):
+    try:
+        with open(f'{working_dir}/{filename}', 'r') as file:
+            contents = file.read()
+        return contents
+    except FileNotFoundError:
+        return "File not found."
+    except IOError:
+        return "Error reading file."
+
+
+def write_todos(message):
+    write_file(message, 'todos.txt')
+
+
+quality_assurance = autogen.AssistantAgent(
+    name="quality_assurance",
+    code_execution_config={},
+    llm_config={
+        "temperature": 0,
+        "request_timeout": 600,
+        "seed": seed,
+        "model": "gpt-3.5-turbo-0613",
+        "config_list": config_list,
+        "functions": [
+            {
+                "name": "open_file",
+                "description": "Read an existing file so that you can determine what else may need to be done",
+                "parameters": {
+                    "type": "object",
+                    "properties": {
+                        "filename": {
+                            "type": "string",
+                            "description": "The name of the file you want to open",
+                        },
+                    },
+                    "required": ["filename"],
+                },
+            },
+            {
+                "name": "write_todos",
+                "description": "Saves your final report by writing a todos.txt file",
+                "parameters": {
+                    "type": "object",
+                    "properties": {
+                        "message": {
+                            "type": "string",
+                            "description": "Your final report containing the todos",
+                        },
+                    },
+                    "required": ["message"],
+                },
+            },
+        ],
+    },
+)
+
+user_proxy = autogen.UserProxyAgent(
+    name="user_proxy",
+    human_input_mode="NEVER",
+    code_execution_config={"work_dir": working_dir},
+    function_map={"open_file": open_file, "write_todos": write_todos},
+)
+
+project_structure = get_files_in_directory(working_dir)
+
+user_proxy.initiate_chat(
+    quality_assurance,
+    message=f"""The dev team had the following task
+
+{initial_prompt}
+
+This is the project structure
+
+{project_structure}
+
+You task is to open these files and inspect the quality of the dev teams work.
+Open all the files to read and adjudicate them.
+Make a todo list of any outstanding work the dev team may be required to complete to accomplish the task.
+Be sure to include the names of the files that need to be edited to accomplish any given todo.
+"""
+)
+
+
